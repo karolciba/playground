da69b70c3e07e015e72e28b6bc8131167729f8e6
Karol Ciba <karociba@amazon.com>
2016-08-09 09:07:54 +0200

    kohonen: circular neuron space

--- kohonen/linear.py
+++ kohonen/linear.py
@@ -14,14 +14,19 @@ class Linear:
         self._length = vector_size
         self._size = map_size
 
+        # self._map = np.zeros( (map_size, vector_size) )
         # self._map = np.random.random( (map_size, vector_size) )
-        self._map = np.random.normal(0.5,0.1, (map_size, vector_size) )
+        self._map = np.random.normal(0.0,0.01, (map_size, vector_size) )
+        # self._map[0] = np.zeros(vector_size)
+        # self._map[map_size-1] = np.ones(vector_size)
+
+        # self._map[:] = [0, 0]
+
         # self._map = np.empty( (map_size, vector_size ) )
-        # self._map[:] = 0.5
 
         self._train = 0
         self._coef = 0.01
-        self._delta = map_size/20
+        self._delta = map_size/10
         self._sim = scipy.signal.ricker(self._delta, (self._delta-1)/2)
         self._sim /= np.max(self._sim)
         # self._sim = scipy.signal.gaussian(self._delta, self._delta/4)
@@ -43,23 +48,35 @@ class Linear:
         score_matrix =  np.linalg.norm(self._map-vector,axis=1)
         bou_index = np.argmin(score_matrix)
 
-        min_x = max(bou_index - self._delta, 0)
-        max_x = min(bou_index + self._delta, self._size)
+        # min_x = max(bou_index - self._delta, 0)
+        # max_x = min(bou_index + self._delta, self._size)
+        min_x = bou_index - self._delta
+        max_x = bou_index + self._delta
 
-        for i in range(min_x, max_x):
-            if i == bou_index and (self._train % 100 == 0):
-                print i, bou_index, vector, self._map[i],
+        for n in range(min_x, max_x):
+            if n < 0:
+                i = self._size + n
+                # print "less", n, i
+            elif n >= self._size:
+                i = (n % self._size) - 1
+                # print "more", n, i
+            else:
+                i = n
 
-            dist = bou_index - abs(i)
+            dist = abs(bou_index - n)
             sim = self._sim[dist - 1]
+
+            if i == bou_index and (self._train % 100 == 0):
+                print n, i, bou_index, dist, sim, vector, self._map[i],
+
 
             # sim = abs(dist/float(self._delta))
             for k in range(0, self._length):
                 # distance = math.sqrt( sum( (a - b)**2 for a,b in zip(vector,self._map[i])))
                 # self._map[i,k] -= self._coef * distance
                 self._map[i,k] -= sim * self._coef * (self._map[i,k] - vector[k])
-                self._map[i,k] = max(self._map[i,k], 0)
-                self._map[i,k] = min(self._map[i,k], 1)
+                # self._map[i,k] = max(self._map[i,k], 0)
+                # self._map[i,k] = min(self._map[i,k], 1)
 
 
 """
@@ -82,12 +99,14 @@ class Visualizer:
         # plt.ion()
         # self._im = plt.imshow(self._som._map)
         self._anim = []
+        self._ims = []
         self._fig = plt.figure()
         self._ax = self._fig.add_subplot(111)
         self._codes = np.empty(self._som._size)
         self._codes[0:self._som._size-1] = Path.LINETO
         self._codes[0] = Path.MOVETO
-        self._codes[self._som._size-1] = Path.STOP
+        self._codes[-1] = Path.STOP
+        # self._codes[-1] = Path.CLOSEPOLY
 
     def show(self):
         # print "Show ", self._som
@@ -97,6 +116,8 @@ class Visualizer:
         path = Path(self._som._map, self._codes)
         patch = patches.PathPatch(path, facecolor='none', lw=1)
 
+        self._ims.append(patch)
+
         self._ax.add_patch(patch)
 
         plt.draw()
@@ -105,35 +126,28 @@ class Visualizer:
         # plt.draw()
         # plt.pause(0.1)
     def animate(self):
-        # print "Animate ", self._som
-        # self.show()
+        print "Animate ", self._som
+        self.show()
         self._anim.append( np.copy(self._som._map) )
-        # self._play()
+        self._play()
     def _play(self):
         for i in self._anim:
             self._im.set_data(i)
             plt.draw()
             plt.pause(0.1)
+
     def save(self, filename):
         import matplotlib
         import matplotlib.animation
         fig = plt.figure()
+        ax = fig.add_subplot(111)
+        ims = self._ims
+        def anim(num):
+            ax.add_patch(ims[num])
+            plt.clf()
 
-        ims = []
-
-        y = 0
-        for i in self._anim:
-            im = plt.imshow(i)
-            # fname = "file%03d.png" % y
-            # y+=1
-            # plt.savefig(fname)
-            ims.append( [im] )
-
-        # Writer = matplotlib.animation.writers['ffmpeg']
-        # writer = Writer(fps=15, metadata=dict(artist='Me'), bitrate=1800)
-        print ims
-
-        im_ani = matplotlib.animation.ArtistAnimation(fig, ims)
+        # im_ani = matplotlib.animation.ArtistAnimation(fig, anim, frames=len(ims))
+        im_ani = matplotlib.animation.FuncAnimation(fig, anim, frames=len(ims))
         # im_ani.save('im.mp4', writer=writer)
         im_ani.save('im.mp4')
 
@@ -143,25 +157,43 @@ class Visualizer:
 
 
 if __name__ == "__main__":
-    som = Linear(2, 300)
+    som = Linear(2, 900)
     vis = Visualizer(som)
-    vis.animate()
+    # vis.animate()
     print ""
+
+    sign = lambda x: (1, -1)[x<0]
+    import math
+    signs = [-1,1]
 
     # for i in range(0,10000):
     i = 0
     while True:
+    # for i in range(0,100):
         i+=1
         if (i%100 == 0):
             print "Train no ", i
             vis.show()
-            # print vector
+            print vector
             # print som._map[0:20]
-        vector = np.random.random(2)
+        # vector = np.random.random(2)
+        # x = np.random.random()
+
+        x = np.random.uniform(0,0.5)
+        ysq = 0.5**2 - x**2
+        y = math.sqrt(ysq)
+
+        xsn = signs[np.random.randint(0,2)]
+        ysn = signs[np.random.randint(0,2)]
+        shx = 0.5 + np.random.normal(0,0.01)
+        shy = 0.5 + np.random.normal(0,0.01)
+        vector = [xsn*x+shx, ysn*y+shy]
+
+        # print vector, math.sqrt(x**2+y**2)
 
         som.train(vector)
 
-    # vis.save('foobar')
+    vis.save('foobar')
 
     # try:
     #     while True:
