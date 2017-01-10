fd9ff5ef9b2a1f2624a27d039ee58c89b521b0b6
Karol Ciba <karociba@amazon.com>
2017-01-10 12:37:10 +0100

    HMM: exercises with HMM algorithms

--- ecg/casino.py
+++ ecg/casino.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 
 from enum import Enum
 
@@ -7,13 +8,15 @@ EMISSIONS = Enum('Toss', 'H T')
 from collections import namedtuple
 Parameters = namedtuple('Parameters','transitions emissions initial')
 
-transitions = { STATES.fair: { STATES.fair: 0.9, STATES.biased: 0.1 },
-                STATES.biased: { STATES.fair: 0.5, STATES.biased: 0.5 } }
-initial = { STATES.fair: 0.83, STATES.biased: 0.17 }
+transitions = { STATES.fair: { STATES.fair: 0.8, STATES.biased: 0.2 },
+                STATES.biased: { STATES.fair: 0.2, STATES.biased: 0.8 } }
+# initial = { STATES.fair: 0.83, STATES.biased: 0.17 }
+initial = { STATES.fair: 0.5, STATES.biased: 0.5 }
 emissions = { STATES.fair: { EMISSIONS.H: 0.5, EMISSIONS.T: 0.5 },
                 STATES.biased: { EMISSIONS.H: 0.75, EMISSIONS.T: 0.25 } }
 
 crooked_casino = Parameters(transitions, emissions, initial)
+
 
 def strtotoss(string):
     l = { 'H': EMISSIONS.H, 'T': EMISSIONS.T }
@@ -60,28 +63,262 @@ def prob_observation_state(x, pi, parameters = crooked_casino):
     # pairs PIi and PIi+1
     state_i, state_i_next = tee(pi)
     state_i_next.next()
+    # (PI1, PI2), (PI2,PI3), ... (PIn-1,PIn)
     trans = izip(state_i, state_i_next)
     # select transitions probabilities
     trans_coef = [ transitions[i][j] for i,j in trans ]
     # states path probability, initial prob * transitions
+    # P(pi)
     state_prob = initial[pi[0]] * mul(trans_coef)
 
-    # import pdb; pdb.set_trace()
+    # list of probabilities for observation i given state_i
     emiss_list = [ emissions[state][obs] for state,obs in zip(pi,x) ]
-    print emiss_list
+    # P(x|pi)
     emiss_prob = mul(emiss_list)
 
+    # end probability is multiplication of pass probability
+    # and emission probability
+    # P(x,pi) = Pi(x|pi)*P(pi)
     return state_prob * emiss_prob
 
-def decode(experiment, parameters = crooked_casino):
+def dynamic_decode(experiment, parameters = crooked_casino):
     transitions = parameters.transitions
     emissions = parameters.emissions
+    initial = parameters.initial
+
+    from collections import defaultdict
+
+    def weight(fro, to, emiss):
+        """ weight of the edge from strate "fro" to state "to" multiplied
+        by emisssion probability "emiss" at the destination state"""
+        return transitions[fro][to] * emissions[to][emiss]
+
+    # Forward pass
+    scores = defaultdict(lambda: 1.0)
+    scores[(-1,STATES.biased)] = initial[STATES.biased], None
+    scores[(-1,STATES.fair)] = initial[STATES.fair], None
+    # for state in list(STATE):
+    #     scores[(state,0)] = 1.0 * emissions[state][experiment[0]]
+
+    for i,o in enumerate(experiment):
+        for state in list(STATES):
+            # import pdb; pdb.set_trace()
+            l = [ (scores[(i-1,s)][0]*weight(s,state,o), s) for s in list(STATES) ]
+            print i,o,state,l,max(l, key= lambda x: x[0])
+            scores[(i,state)] = max(l, key= lambda x: x[0])
+
+    filtered = [ [ (k[1],v) for k,v in scores.items() if k[0] == index ] for index in xrange(len(experiment)) ]
+    maxes = [ max(l, key=lambda x:x[1]) for l in filtered ]
+    return maxes, scores
+
+    return scores
+
+def forward(observations, parameters = crooked_casino):
+    transitions = parameters.transitions
+    emissions = parameters.emissions
+    initial = parameters.initial
+
+    # P(state_i, observation_1:i)
+    # for P(state_i | observation_1:i) normalize by sum of P(state_i, observation_1:i)
+    scores = [ {} for x in range(len(observations)) ]
+
+    scores[0] = { state: initial[state]*emissions[state][observations[0]] for state in list(STATES) }
+    # nomalize border case
+    # s = sum(v for v in scores[0].values())
+    # scores[0] = { k: v/s for k,v in scores[0].items() }
+
+    # P(state_i, observation_1:i) = SIGMA(state_i-1) P(o_i|state_i) * P(state_i
+    # | state_i-1) * P(state_i-1, observation_1:i-1)
+    for i,o in enumerate(observations[1:],1):
+        for state in list(STATES):
+            prev = 0.0
+            for prev_state in list(STATES):
+                prev += transitions[prev_state][state] * scores[i-1][prev_state]
+            scores[i][state] = emissions[state][o] * prev
+        # scale = sum(v for v in scores[i].values())
+        # for state in list(STATES):
+        #     scores[i][state] /= scale
+
+    # f = scores
+    # sums = [ sum (v for v in ff.values()) for ff in f ]
+    # norm = [ { k:v/s for k,v in ff.items() } for ff,s in zip(f,sums) ]
+
+    return scores
+
+def backward(observations, parameters = crooked_casino):
+    transitions = parameters.transitions
+    emissions = parameters.emissions
+    initial = parameters.initial
+
+    scores = [ {} for x in range(len(observations)+1) ]
+
+    # scores[-1] = { state: emission[state][observation[-1]] * transition[state
+    scores[-1] = { state: 1.0 for state in list(STATES) }
+
+    # P(observation_i+1 | state_i) = SIGMA(state_i+1) P(o_i+2:n | state_i+1) *
+    # P(o_i+1 | state_i+1) * P(state_i+1 | state_i)
+    for i,o in reversed(list(enumerate(observations))):
+        for state in list(STATES):
+            nxt = 0.0
+            # print i,o,scores
+            # import pdb; pdb.set_trace()
+            for next_state in list(STATES):
+                nxt += scores[i+1][next_state] * transitions[state][next_state]
+            scores[i][state] = nxt * emissions[state][o]
+
+    return scores
+
+def forward_backward(observations, parameters = crooked_casino):
+    f = forward(observations, parameters)
+    b = backward(observations, parameters)
+
+    fb = [ {} for x in f ]
+    for i,row in enumerate(f):
+        # import pdb; pdb.set_trace()
+        s = 0.0
+        for k,v in row.items():
+            fb[i][k] = v * b[i][k]
+            # if fb[i][k] == 0:
+            #     print "!!forward backward!! epsilon"
+            #     fb[i][k] = 0.01
+            s += fb[i][k]
+        # normalize
+        for k,v in row.items():
+            fb[i][k] /= s
+
+    return fb
+
+def max_forward_backward(observations, parameters = crooked_casino):
+    fb = forward_backward(observations, parameters)
+
+    return [ max(row.items(), key = lambda x: x[1]) for row in fb ]
+
+
+from random import random
+
+x = random(); y = 1 - x;
+a = random(); b = 1 - a;
+model_transitions = { STATES.fair: { STATES.fair: a, STATES.biased: b },
+                STATES.biased: { STATES.fair: x, STATES.biased: y } }
+# initial = { STATES.fair: 0.83, STATES.biased: 0.17 }
+x = random(); y = 1 - x;
+model_initial = { STATES.fair: x, STATES.biased: y }
+x = random(); y = 1 - x;
+a = random(); b = 1 - a;
+model_emissions = { STATES.fair: { EMISSIONS.H: a, EMISSIONS.T: b },
+                STATES.biased: { EMISSIONS.H: x, EMISSIONS.T: y } }
+
+casino_model = Parameters(model_transitions, model_emissions, model_initial)
+
+def train(no = 10, l = 10):
+    print "Before"
+    print "Trans", casino_model.transitions
+    print "Emis", casino_model.emissions
+    print "Init", casino_model.initial
+    import sys
+    for t in xrange(no):
+        g = casino()
+        obs = [ g.next().emission for x in xrange(l) ]
+        print "\r","Train", t,
+        sys.stdout.flush()
+        # print "Observations", obs
+        m = baum_welch(obs)
+    print ""
+    print "After"
+    print "Trans", m.transitions
+    print "Emis", m.emissions
+    print "Init", m.initial
+    print ""
+    print "Orig"
+    print "Trans", crooked_casino.transitions
+    print "Emis", crooked_casino.emissions
+    print "Init", crooked_casino.initial
+
+
+def baum_welch(observations, model = casino_model):
+    transitions = model.transitions
+    emissions = model.emissions
+    initial = model.initial
+
+    f = forward(observations, model)
+    b = backward(observations, model)
+
+    # print "forward", f
+    # print "backward", b
+
+
+    ksi = [ { fro: { to: 1.0 for to in list(STATES) } for fro in list(STATES) } for x in observations[:-1] ]
+    for i,o in enumerate(observations[1:] ,0):
+        s = 0.0
+        for fro in list(STATES):
+            for to in list(STATES):
+                ksi[i][fro][to] = f[i][fro]*transitions[fro][to]*b[i+1][to]*emissions[to][o]
+                # if ksi[i][fro][to] == 0:
+                #     print "!!baum welch!! epsilon"
+                #     ksi[i][fro][to] = 0.01
+                #     # import pdb; pdb.set_trace()
+                s += ksi[i][fro][to]
+        # normalize
+        # if s != 0:
+        if True:
+            for fro in list(STATES):
+                for to in list(STATES):
+                    ksi[i][fro][to] /= s
+
+    # gamma = forward_backward(observations, model)
+    # print "old gamma", len(gamma), gamma
+
+    gamma = [ { state: 1.0 for state in list(STATES) } for x in observations[:-1] ]
+    for i,row in enumerate(gamma):
+        for state in list(STATES):
+            s = sum(ksi[i][state][to] for to in list(STATES))
+            row[state] = s
+
+    # print "new gamma", len(gamma), gamma
+    # print ksi
+
+    for row in gamma:
+        for state in list(STATES):
+            initial[state] = row[state]
+    s = sum(v for v in initial.values())
+    for state in initial:
+        initial[state]/=s
+
+    for fro in list(STATES):
+        for to in list(STATES):
+            transitions[fro][to] = sum( row[fro][to] for row in ksi[:-1] )/sum( row[fro] for row in gamma[:-1])
+            if transitions[fro][to] == 0:
+                transitions[fro][to] = 0.01
+
+    for state in list(STATES):
+        for em in list(EMISSIONS):
+            nom = 0.0
+            for i in xrange(len(observations[:-1])):
+                if observations[i] == em:
+                    nom += gamma[i][state]
+            emissions[state][em] = nom/sum( row[state] for row in gamma)
+            if emissions[state][em] == 0:
+                emissions[state][em] = 0.01
+
+    return model
+
+def decode(experiment, parameters = crooked_casino, verbose = True):
+    """ Calculates most probable path which gives observed emissions => argmax(P(x,PI))
+    x - observed emissions
+    PI - path throught hidden states
+
+    Exponential decode, iterates over all paths calculating it's probabilities
+    O(len(experiment),states) = states**len(experiments)
+    """
+    transitions = parameters.transitions
+    emissions = parameters.emissions
+    initial = parameters.initial
 
     from itertools import product
     paths = product(list(STATES), repeat=len(experiment))
     paths_prob = {}
     for path in paths:
-        prob = 1./len(STATES)
+        prob = initial[path[0]]
         prev = path[0]
         for state in path[1:]:
             prob *= transitions[prev][state]
@@ -96,22 +333,24 @@ def decode(experiment, parameters = crooked_casino):
         paths_emiss_prob[path] = emiss_prob
 
     arg_max = max(paths_emiss_prob.items(), key=lambda x: x[1])
-    print prettyprint(arg_max[0],arg_max[1])
-    print "------------"
-    for k,v in sorted(paths_emiss_prob.items(), key=lambda l: l[1]):
-        print prettyprint(k,v)
+    if verbose:
+        print prettyprint(arg_max[0],arg_max[1])
+        print "------------"
+        for k,v in sorted(paths_emiss_prob.items(), key=lambda l: l[1]):
+            print prettyprint(k,v)
 
     return arg_max
 
 def pass_prob(experiment, parameters = crooked_casino):
     transitions = parameters.transitions
     emissions = parameters.emissions
+    initial = parameters.initial
 
     from itertools import product
     paths = product(list(STATES), repeat=len(experiment))
     paths_prob = {}
     for path in paths:
-        prob = 1./len(STATES)
+        prob = initial[path[0]]
         prev = path[0]
         for state in path[1:]:
             prob *= transitions[prev][state]
@@ -183,3 +422,9 @@ def estimation(gen = casino(), samples=1000):
         normalized_emissions[state] = normalized_row
 
     return Parameters(normalized_transitions, normalized_emissions)
+
+if __name__ == '__main__':
+    for i in range(100):
+        print "Batch no", i
+        train(100,300)
+        print ""
